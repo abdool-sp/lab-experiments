df54178ff838b206a50fff7472ea5b7f4950790c
abdool-sp <abdulsalamu6@gmail.com>
2019-03-13 15:21:39 +0530

    edit led blinking

--- led_blinking.py
+++ led_blinking.py
@@ -1,9 +1,10 @@
 from machine import Pin
 
-p0 = Pin(0, Pin.OUT)    # create output pin on GPIO0
+led = Pin(2, Pin.OUT)    # create output pin on GPIO0
 
 
 while True:
-   p0.value(1)
+   led.value(1)
    sleep(1)
-   p0.value(0)
+   led.value(0)
+   sleep(1)
